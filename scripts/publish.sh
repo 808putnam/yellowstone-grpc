b4948dd4f5000ad31c738d8ded112ba3aa16e49d
Bill McRoberts <111695998+808putnam@users.noreply.github.com>
2024-10-19 17:28:39 +0000

    chore: adding back in publish

---
+++ scripts/publish.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+FULL_VERSION=$1
+
+# TODO: Add back in when we can start using v prefixes
+# if [[ ${FULL_VERSION} != v* ]]; then
+#   echo "Version must start with 'v'"
+#   exit 1
+# fi
+
+git tag -a "${FULL_VERSION}"
+git push origin "${FULL_VERSION}"
